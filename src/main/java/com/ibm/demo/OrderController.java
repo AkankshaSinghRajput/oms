a99f10a9fca99c5e7c9b99755e892bf8c740e95e
unknown <ibmjavanh05@iiht.tech>
2021-03-25 17:40:29 +0530

    Refactored the code

--- src/main/java/com/ibm/demo/OrderController.java
+++ src/main/java/com/ibm/demo/OrderController.java
@@ -1,43 +1,67 @@
 package com.ibm.demo;
+
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ibm.demo.entity.Order;
-@RestController
-public class OrderController {
-	@Autowired
-	OrderService orderService;
+import com.ibm.demo.service.OrderService;
+
+@RestController // Bean
+public class OrderController { // frontend
+	@Autowired // is used for DI
+	OrderService orderService; // DI
+
 	@PostMapping("/order")
+	@ResponseStatus(code = HttpStatus.CREATED)
 	String createOrder(@RequestBody @Valid Order order, BindingResult bindingResult) {
-		if(bindingResult.hasErrors()) {
-			throw new IllegalArgumentException();
-		}
+		validateModel(bindingResult);
 		System.out.println(order);
-		return "success";
+		return orderService.createOrder(order); // delegate
 	}
+
 	@GetMapping("/order")
-	String getOrder() {
-		return "order created";	
+	List<Order> getOrders() {
+		return orderService.getOrders();
 	}
+
+	@GetMapping("/order/{id}")
+	Order getOrder(@PathVariable("id") int orderId) {
+		return orderService.getOrder(orderId);
+	}
+
+	private void validateModel(Errors bindingResult) {
+		if (bindingResult.hasErrors()) {
+			throw new IllegalArgumentException("Somethign went wrong. Plesae retry");
+		}
+	}
+
+//DRY 
 	@PutMapping("/order/{id}")
-	String updateOrder(@PathVariable("id") int orderId) {
+	void updateOrder(@RequestBody @Valid Order order, BindingResult bindingResult, @PathVariable("id") int orderId) {
+		validateModel(bindingResult);
 		System.out.println(orderId);
-		return "Order Update";	
+		orderService.updateOrder(orderId);
 	}
+
 	@DeleteMapping("/order/{id}")
-	String deleteOrder(@PathVariable("id") int orderId) {
+	void deleteOrder(@PathVariable("id") int orderId, HttpRequest httpRequest) {
+		System.out.println(httpRequest.getHeaders());
 		System.out.println(orderId);
-		return "Order deleted";
-		
+		orderService.deleteOrder(orderId);
 	}
-	
 }
