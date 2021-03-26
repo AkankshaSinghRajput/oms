8e3c69d0ab9e28b65586f8ee14e0920104a50cd2
unknown <ibmjavanh05@iiht.tech>
2021-03-26 11:34:39 +0530

    implemented createOrder with mongodb

--- src/main/java/com/ibm/demo/service/OrderService.java
+++ src/main/java/com/ibm/demo/service/OrderService.java
@@ -3,14 +3,20 @@ package com.ibm.demo.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.ibm.demo.entity.Order;
+import com.ibm.demo.repo.OrderRepository;
 
 @Service
 public class OrderService { // Spring Beans
-	public String createOrder(Order order) {
-		return "order created";
+	@Autowired
+	OrderRepository orderRepository;
+	public String createOrder(@RequestBody Order order) {
+		Order savedOrder = orderRepository.save(order);
+		return savedOrder.getId();
 	}
 
 	public List<Order> getOrders() {
